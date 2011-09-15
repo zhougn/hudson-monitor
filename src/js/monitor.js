4bed7ebe474cc87a6f2fbceaad05052bf29b835e
Neng <zhougn@gmail.com>
2011-09-15 11:22:04 +0800

    play sound when build success or fail

--- src/js/monitor.js
+++ src/js/monitor.js
@@ -26,7 +26,7 @@ var Monitor = new Class({
                     if(!monitoringJobs.contains(job.name)) {
                         return;
                     }
-                    var jobMonitor = new JobMonitor(job);
+                    var jobMonitor = new JobMonitor(job.name, job.url);
                     $jobs.append(jobMonitor.$dom);
                     self._jobMonitors.push(jobMonitor);
                 });
@@ -51,26 +51,56 @@ var JobStatuses = {
 };
 
 var JobMonitor = new Class({
-    initialize: function(job) {
-        this._job = job;
+    _jobEvents: {
+        build_fail: function(oldJob, newJob) {
+            return JobStatuses[oldJob.color] == 'building' && JobStatuses[newJob.color] == 'fail';
+        },
+        build_success: function(oldJob, newJob) {
+            return JobStatuses[oldJob.color] == 'building' && JobStatuses[newJob.color] == 'success';
+        }
+    },
+
+    initialize: function(name, url) {
+        this._url = url + 'api/json';
+        this._name = name;
+
         this._buildUI();
+
+        this.refresh();
     },
 
     refresh: function() {
         var self = this;
         jQuery.ajax({
-            url: this._job.url,
+            url: this._url,
             dataType: 'json',
             success: function(job) {
-                self._job = job;
-                self.refreshUI();
+                self._refreshJob(job);
+                self._refreshUI();
             }
         });
     },
 
     _buildUI: function() {
-        var template = '<li class="job {status}">{name}</li>';
-        this.$dom = $(template.substitute({status:this._status(), name:this._job.name}));
+        var template = '<li class="job">{name}</li>';
+        this.$dom = $(template.substitute({name:this._name}));
+    },
+
+    _refreshJob: function(newJob) {
+        if(!!this._job) {
+            var oldJob = this._job;
+            var self = this;
+            Object.each(this._jobEvents, function(condition, eventName) {
+                if(condition(oldJob, newJob)) { self._notify(eventName); }
+            });
+        }
+
+        this._job = newJob;
+    },
+
+    _notify: function(eventName) {
+        var audio = document.getElementById('audio_' + eventName);
+        audio.play();
     },
 
     _refreshUI: function() {
