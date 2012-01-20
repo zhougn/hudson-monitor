43e37d82ef588949e1fb690b2500daaebb5d38d3
Neng <zhougn@gmail.com>
2012-01-21 01:06:46 +0800

    create vendor js folder

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -1,5 +1,6 @@
-require('/js/jquery-1.7.1.min.js');
-require('/js/sugar-1.1.3.min.js');
-require('/js/chocolate.js');
+require('/js/vendor/jquery-1.7.1.min.js');
+require('/js/vendor/jquery.tmpl.min.js');
+require('/js/vendor/sugar-1.1.3.min.js');
+require('/js/vendor/chocolate.js');
 require('/js/monitor.js');
 require('/js/options.js');
